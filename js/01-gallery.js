f75575564c27cdd0beb1136a7e6405e6a89d48d1
ZemskovAlexander <zemskov25121992@gmail.com>
2022-08-06 17:07:29 +0300

    fix

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,36 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galaryList = document
-  .querySelector(".gallery")
-  .insertAdjacentHTML("beforebegin", `<div class="gallery"></div>`);
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCardsMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+galleryContainer.addEventListener("click", onGalleryContainerClick);
+
+function createGalleryCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a class="gallery__link" href="${original}" onclick="return false;">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+      </a>
+    </div>
+  `;
+    })
+    .join("");
+}
+
+function onGalleryContainerClick(evt) {
+  if (!evt.target.classList.contains("gallery__item")) {
+    return;
+  }
+
+  console.log(evt.target);
+}
