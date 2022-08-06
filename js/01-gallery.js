6411f859b44fb41acadac9c7e40429f0c8ac2833
ZemskovAlexander <zemskov25121992@gmail.com>
2022-08-06 19:41:14 +0300

    may fix src img

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,6 +1,13 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const modalTemplate = (img) =>
+  `
+  <div class="modal">
+    <img src=${img}>
+    </div>
+  `;
+
 const galleryContainer = document.querySelector(".gallery");
 const cardsMarkup = createGalleryCardsMarkup(galleryItems);
 
@@ -28,9 +35,20 @@ function createGalleryCardsMarkup(galleryItems) {
 }
 
 function onGalleryContainerClick(evt) {
+  basicLightbox
+    .create(
+      `
+    <img src="${galleryItems.original}">
+`
+    )
+    .show();
+
   if (!evt.target.classList.contains("gallery__item")) {
     return;
   }
 
   console.log(evt.target);
+  // modalTemplate();
 }
+
+// const instance = basicLightbox.create(document.querySelector(".modal")).show();
